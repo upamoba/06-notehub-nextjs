eccf23c2884a428b2d85bfa82c9f1a1fe1c1d6f2
uty <fgmapam@gmail.com>
2025-07-30 16:09:30 +0300

    Summary(required)

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,11 +1,24 @@
 import { dehydrate, QueryClient } from '@tanstack/react-query';
-
 import { fetchNoteById } from '../../../lib/api';
 import NoteDetailsClient from './NoteDetails.client';
+import { TanStackProvider } from '../../../components/TanStackProvider/TanStackProvider';
 
-export default async function NoteDetailsPage({ params }: { params: { id: string } }) {
-  const id = Number(params.id);
+export default async function NoteDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const noteId = Number(params.id);
   const qc = new QueryClient();
-  await qc.prefetchQuery({ queryKey: ['note', id], queryFn: () => fetchNoteById(id) });
-  return <NoteDetailsClient dehydratedState={dehydrate(qc)} noteId={id} />;
+  await qc.prefetchQuery({
+    queryKey: ['note', noteId],
+    queryFn: () => fetchNoteById(noteId),
+  });
+  const dehydratedState = dehydrate(qc);
+
+  return (
+    <TanStackProvider dehydratedState={dehydratedState}>
+      <NoteDetailsClient noteId={noteId} />
+    </TanStackProvider>
+  );
 }
